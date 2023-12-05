7cf8836f21e8b1f5726a71eb438eba774076256a
unknown <siwild81@hotmail.com>
2023-12-05 14:54:03 +0000

    deleted C# files

--- C#Files/GigData.cs
+++
@@ -1,9 +0,0 @@
-
-record GigData(
-    string Artist,
-    string Genre,
-    string Date,
-    string Location, 
-    string Venue,
-    double TicketPrice
-);
--- C#Files/Program.cs
+++
@@ -1,4 +0,0 @@
-﻿
-var newGig = new GigData("Daft Punk","Electronic","24th Feb 2024","London","The O2", 89.50);
-Console.WriteLine(newGig);
-
